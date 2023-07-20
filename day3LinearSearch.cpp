32b3329e668077e53373ce0739b09da6e51a31df
Ajay Singh Mehta <ajay.mehta332005@gmail.com>
2023-07-20 14:51:02 +0530

    third commit

---
+++ day3LinearSearch.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int linearSearch(vector<int> v, int n,int index)
+{
+    if(v[index] == n ) 
+        return index;
+    if(index == v.size()-1)
+        return -1;
+    return linearSearch(v,n,index+1);
+}
+
+int main()
+{
+    vector<int> v = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    int n ; 
+    cin>> n;
+    cout<<linearSearch(v,n,0);
+    return 0;
+}
