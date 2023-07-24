2dc2e51e810cfda22b57031c0b7da3a809399b27
Ajay Singh Mehta <ajay.mehta332005@gmail.com>
2023-07-24 17:01:00 +0530

    ninth commit

---
+++ day5mergeSort.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void merge(vector<int>& v, int start, int end)
+{
+    int mid = (start + end) / 2;
+
+    int size1 = mid - start + 1;
+    int size2 = end - mid;
+
+    vector<int> arr1(size1);
+    vector<int> arr2(size2);
+
+    for (int i = 0; i < size1; ++i)
+    {
+        cout << "copying in array1: " << v[start + i] << endl;
+        arr1[i] = v[start + i];
+    }
+
+    for (int i = mid + 1; i <= end; ++i)
+    {
+        cout << "copying in array2: " << v[i] << endl;
+        arr2[i - (mid + 1)] = v[i];
+    }
+
+    int idx1 = 0, idx2 = 0;
+    int vidx = start;
+
+    while (idx1 < size1 && idx2 < size2)
+    {
+        if (arr1[idx1] <= arr2[idx2])
+        {
+            v[vidx++] = arr1[idx1++];
+        }
+        else
+        {
+            v[vidx++] = arr2[idx2++];
+        }
+    }
+
+    while (idx1 < size1)
+    {
+        v[vidx++] = arr1[idx1++];
+    }
+
+    while (idx2 < size2)
+    {
+        v[vidx++] = arr2[idx2++];
+    }
+}
+
+void mergeSort(vector<int> &v, int start, int end)
+{
+    if (start >= end)
+        return;
+    int mid = (start + end) / 2;
+    cout << "mergeSort is called from " << start << " to " << end << endl;
+    // Recursively sort first and second halves of the array
+    mergeSort(v, start, mid);
+
+    // Recursively sorting second half of the array
+    mergeSort(v, mid + 1, end);
+
+    /* Merge both sorted subarrays */
+    merge(v, start, end);
+}
+
+int main()
+{
+    vector<int> v = {1, 2, 3, 5, 4, 9, 8, 7, 69, 10};
+    mergeSort(v, 0, v.size() - 1);
+    for (auto a : v)
+        cout << a << " ";
+    return 0;
+}
